0952a98b93b96198d060760dc6c2866416e83101
BlackStar54213 <113902603+BlackStar54213@users.noreply.github.com>
2024-10-26 15:18:42 +0100

    Update dude.js

--- dude.js
+++ dude.js
@@ -1,7 +1,1 @@
-function able(chosen, me) {
-    let add = chosen + me;
-    let minus = chosen - me;
-    let div = chosen / me;
-    let times = chosen * me;
-    console.log(add + ' ' + minus + ' ' + div.toFixed(2) + ' ' + times);
-} able(34, 87);
+fja'pdfadfadfa
